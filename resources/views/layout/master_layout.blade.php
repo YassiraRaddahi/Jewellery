49600dd7f55b141a4162e7c89dac9b3b92cad6d5
Lizapetrushenko <liza2511liza@gmail.com>
2025-05-14 10:23:50 +0200

    master_layout gemaakt

---
+++ resources/views/layout/master_layout.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script src="https://kit.fontawesome.com/5c3b708ee0.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+    <header>
+       <div id="foto">
+        <a href="#">
+            <img src="images/Logo.png" alt="logo">
+        </a>
+       </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Our story</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    
+        <div class="icon-buttons">
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-magnifying-glass fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-cart-shopping fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa-regular fa-circle-user fa-lg"></i></a></li>
+            </ul>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+    
+        <p>&copy; 2025 Moonie Studio</p>
+        <li><a href="#">Privacy Policy</a></li>
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.master_layout');
 });
+
+// Route::get('/account', function () {
+//     return view('pages.account');
+// })->name('account');
+
+
+// Route::get('/home', [HomeController::class, 'index'])->name('home');
+// Route::get('/about', [AboutController::class, 'index'])->name('story');
+// Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+
+// Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+// Route::get('/privacy', [PrivacyController::class, 'index'])->name('privacy');
+// Route::get('/cart', [CartController::class, 'index'])->name('cart');
+// Route::get('/search', [SearchController::class, 'index'])->name('search');
+
+
