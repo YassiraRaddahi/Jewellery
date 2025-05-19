0ec44f0c188d515bff75e214fa9ae2e63015c7ff
YassiraRaddahi <72447342+YassiraRaddahi@users.noreply.github.com>
2025-05-19 13:14:26 +0200

    verder gegaan met styling

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,27 +5,32 @@
 
 @section('content')
 
-<div class="products-container">
-    @foreach($products as $product)
-        <div class="product-category-container">
-            <div class="product-category">
-                <a href="{{route('products.show', $product->id)}}"><img src="{{asset($product->image)}}" alt="{{$product->name}}"></a>
-            </div>    
-
-            <div class="product-category-name-price">
-                <div><h4>{{$product->name}}</h4></div>
-
-                <div class="product-category-price">
-                @if($product->on_sale)
-                    <p>€{{number_format($product->price * (1 - $product->discount_factor), 2)}}</p>
-                    <p class="product-old-price">€{{number_format($product->price, 2)}}</p>
-                @else
-                    <p>€{{number_format($product->price, 2)}}</p>
-                @endif
-                </div>
-            </div>
+<div class="product-show">   
+    <div class="product-category-container">
+        <div class="product-category">
+            <img src="{{asset($product->image)}}" alt="{{$product->name}}">
+        </div>    
+        <div class="product-category-name-price">
+            <div><h4>{{$product->name}}</h4></div>    
         </div>
-    @endforeach
-</div>
+    </div>
 
+    <div class="product-show-information">
+        <div class="product-show-description">
+            <h2>Description</h2>
+            <p>{{$product->description}}</p>
+        </div>
+        <div class="product-category-price">
+            @if($product->on_sale)
+                <p>€{{number_format($product->price * (1 - $product->discount_factor), 2)}}</p>
+                <p class="product-old-price">€{{number_format($product->price, 2)}}</p>
+            @else
+                <p>€{{number_format($product->price, 2)}}</p>
+            @endif
+        </div>
+        <div>
+            
+        </div>
+    </div>
+</div>
 @endsection
