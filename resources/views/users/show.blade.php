dd0a6adc312de21d7d57c82dfbf4795a53f5d779
BBakkerr <beabakkerr@gmail.com>
2025-05-22 09:38:48 +0200

    welcome_user erbij gezet

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,3 +1,12 @@
 @extends('layout.master_layout')
 @section('title', 'accountpage')
 @section('content')
+<div class="welcome_user">
+    <h1>Welcome {{ $user->name }} to our team</h1>
+    <h2>We appreciate you for choosing us!</h2>
+    
+    <div id="icon_welcome">
+        <img src="">
+    </div>
+</div>
+@endsection
