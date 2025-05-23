1a9d7e62994226a5f9a28103ecf1c8ea5c7f1102
BBakkerr <beabakkerr@gmail.com>
2025-05-23 09:38:03 +0200

    account gewijzigd

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,9 @@
     <h2>We appreciate you for choosing us!</h2>
     
     <div id="icon_welcome">
-        <img src="">
+        <img src="{{ asset('images/icons/personal_data.png') }}" alt="Personal Data Icon">
+        <img src="{{ asset('images/icons/Cart_Order_History.png') }}" alt="Order History Icon">
+        <img src="{{ asset('images/icons/log-out.png') }}" alt="Logging Out Icon">
     </div>
 </div>
 @endsection
