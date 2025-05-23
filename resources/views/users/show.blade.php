6b939c00a6905f32aac2c1a9bd4ddc3837c8e121
BBakkerr <beabakkerr@gmail.com>
2025-05-23 11:03:30 +0200

    column

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,7 +7,7 @@
     <h1>Welcome (username) to our team</h1>
     <h2>We appreciate you for choosing us!</h2>
     
-    <div id="icon_welcome">
+    <div id="icon_welcome"> 
         <img src="{{ asset('images/icons/personal_data.png') }}" alt="Personal Data Icon">
         <img src="{{ asset('images/icons/Cart_Order_History.png') }}" alt="Order History Icon">
         <img src="{{ asset('images/icons/log-out.png') }}" alt="Logging Out Icon">
