1f0d86571e1d673075459ef258e6a31b56bc02ec
BBakkerr <beabakkerr@gmail.com>
2025-05-23 09:38:34 +0200

    verkeerd gezet

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,14 +1,5 @@
 @extends('layout.master_layout')
 @section('title', 'accountpage')
 @section('content')
-<div class="welcome_user">
-    <h1>Welcome {{ $user->name }} to our team</h1>
-    <h2>We appreciate you for choosing us!</h2>
-    
-    <div id="icon_welcome">
-        <img src="{{ asset('images/icons/personal_data.png') }}" alt="Personal Data Icon">
-        <img src="{{ asset('images/icons/Cart_Order_History.png') }}" alt="Order History Icon">
-        <img src="{{ asset('images/icons/log-out.png') }}" alt="Logging Out Icon">
-    </div>
-</div>
+
 @endsection
