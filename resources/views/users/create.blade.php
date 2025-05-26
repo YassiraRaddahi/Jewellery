e8b84390ffcbad9afe1b986888cf7de7f3fd7ea6
Lizapetrushenko <liza2511liza@gmail.com>
2025-05-26 11:47:49 +0200

    styling verbeterd + misplaatste tag weggehaald

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,40 +6,35 @@
 <div class="create_account_container">
     <h1>Create Account</h1>
     <form action="{{ route('users.create') }}" method="POST">
-    @csrf
+        @csrf
 
-    <div>
-        <label for="name">First Name:</label>
-        <input type="text" id="name" name="name" required>
-    </div>
-    
-    <div>
-        <label for="insertion">Insertion:</label>
-        <input type="text" id="insertion" name="insertion" required>
-    </div>
+        <div>
+            <label for="name">First Name:</label>
+            <input type="text" id="name" name="name" required>
+        </div>
 
-    <div>
-        <label for="surname">Last Name:</label>
-        <input type="text" id="surname" name="surname" required>
-    </div>
+        <div>
+            <label for="insertion">Insertion:</label>
+            <input type="text" id="insertion" name="insertion" required>
+        </div>
 
-    <div>
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" required>
-    </div>
+        <div>
+            <label for="surname">Last Name:</label>
+            <input type="text" id="surname" name="surname" required>
+        </div>
 
-    <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-    </div>
-    <div id ="button_create_account">
-       <button type="submit">Create</button>
-    </div>
+        <div>
+            <label for="email">Email:</label><br>
+            <input type="email" id="email" name="email" required>
+        </div>
 
-    
-</form>
-
-        
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div id ="button_create_account">
+            <button type="submit">Create</button>
+        </div>
     </form>
 
     <p>Already have an account? <a href="{{ route('login') }}">Log in here</a></p>
