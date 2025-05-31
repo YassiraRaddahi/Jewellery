8c21b1a136cf2ce8f2e762188fa2e86bd8ec0be0
YassiraRaddahi <72447342+YassiraRaddahi@users.noreply.github.com>
2025-06-01 00:05:36 +0200

    login route gewijzigd + naam login view veranderd en naar users mapje verplaatst

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,10 @@ Route::get('/contact', function () {
     return view('pages.contact');
 })->name('contact');
 
-Route::get('/login', function () {
-    return view('pages.log_in');
+Route::get('/users/login', function () {
+    return view('users.login');
 })->name('login');  
+
 
 Route::get('/categories/{name}', [CategoriesController::class, 'show'])->name('categories.show');
 Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
