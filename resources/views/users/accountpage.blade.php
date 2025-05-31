2a2cbb9e689f8046fd78e2edbdfc4af67e89aa8d
YassiraRaddahi <72447342+YassiraRaddahi@users.noreply.github.com>
2025-06-01 00:01:21 +0200

    bijbehorende text onder de icons gezet

--- resources/views/users/accountpage.blade.php
+++ resources/views/users/accountpage.blade.php
@@ -9,9 +9,18 @@
         <h2>We appreciate you for choosing us!</h2>
     </div>
     <div class="icons-general-accountpage-container">
-        <a href="#"><img id="order-history-icon" src="{{ asset('images/icons/Cart_Order_History.png') }}" alt="Order History Icon"></a>
-        <a href="#"><img id="personal-data-icon" src="{{ asset('images/icons/personal_data.png') }}" alt="Personal Data Icon"></a>
-        <a href="#"><img id="logout-icon"src="{{ asset('images/icons/log-out.png') }}" alt="Logging Out Icon"></a>
+        <div>
+            <a href="#"><img id="order-history-icon" src="{{ asset('images/icons/Cart_Order_History.png') }}" alt="Order History Icon"></a>
+            <p>Order History</p>
+        </div>
+        <div>
+            <a href="#"><img id="personal-data-icon" src="{{ asset('images/icons/personal_data.png') }}" alt="Personal Data Icon"></a>
+            <p>Personal Data</p>
+        </div>
+        <div>
+            <a href="#"><img id="logout-icon"src="{{ asset('images/icons/log-out.png') }}" alt="Logging Out Icon"></a>
+            <p>Log Out</p>
+        </div>
     </div>
 </div>
 @endsection
