8044f79407569ba56c31fff2f2b11614375bce7d
BBakkerr <beabakkerr@gmail.com>
2025-06-04 21:16:22 +0200

    gewijzigd

--- resources/views/layout/master_layout.blade.php
+++ resources/views/layout/master_layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <script src="https://kit.fontawesome.com/5c3b708ee0.js" crossorigin="anonymous"></script>
