dade44a4968a3734548f0e97b36c1316fc95f5c2
YassiraRaddahi <72447342+YassiraRaddahi@users.noreply.github.com>
2025-06-06 22:52:34 +0200

    general accountpage styling verbeterd + delete account knop toegevoegd

--- resources/views/users/accountpage.blade.php
+++ resources/views/users/accountpage.blade.php
@@ -9,18 +9,32 @@
         <h2>We appreciate you for choosing us!</h2>
     </div>
     <div class="icons-general-accountpage-container">
-        <div>
-            <a href="#"><img id="order-history-icon" src="{{ asset('images/icons/Cart_Order_History.png') }}" alt="Order History Icon"></a>
-            <p>Order History</p>
-        </div>
-        <div>
-            <a href="#"><img id="personal-data-icon" src="{{ asset('images/icons/personal_data.png') }}" alt="Personal Data Icon"></a>
-            <p>Personal Data</p>
-        </div>
-        <div>
-            <a href="#"><img id="logout-icon"src="{{ asset('images/icons/log-out.png') }}" alt="Logging Out Icon"></a>
-            <p>Log Out</p>
-        </div>
+        <a href="#">
+            <div>
+                <img id="order-history-icon" src="{{ asset('images/icons/Cart_Order_History_2.0.png') }}" alt="Order History Icon">
+                <p>Order History</p> 
+            </div>
+        </a>
+        <a href="#">
+            <div>
+                <img id="personal-data-icon" src="{{ asset('images/icons/personal_data_2.0.png') }}" alt="Personal Data Icon">
+                <p>Personal Data</p>
+            </div>
+        </a>
+        <a href="#">
+            <div>
+                <img id="delete-account-icon"src="{{ asset('images/icons/delete-account.png') }}" alt="Delete Account Icon">
+                <p>Delete Account</p>
+            </div>
+        </a>
+        <a href="#">
+            <div>
+                <div id="logout-icon-container">
+                    <img id="logout-icon"src="{{ asset('images/icons/log-out.png') }}" alt="Logging Out Icon">
+                </div>
+                <p>Log Out</p>
+            </div>
+        </a>
     </div>
 </div>
 @endsection
