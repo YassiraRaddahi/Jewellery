3c4b1436e7b5fae20170bc408f6a960792ec2269
BBakkerr <beabakkerr@gmail.com>
2025-06-13 11:46:35 +0200

    a

--- public/js/script.js
+++ public/js/script.js
@@ -11,3 +11,14 @@ function ProductAddedMessage(button)
     }, 1000);
 
 }
+document.addEventListener('DOMContentLoaded', function () {
+        const icon = document.getElementById('show-search');
+        const container = document.getElementById('search-form-container');
+        const input = document.getElementById('search-input');
+
+        icon.addEventListener('click', function (e) {
+            e.preventDefault();
+            container.style.display = 'block';
+            input.focus();
+        });
+    });
