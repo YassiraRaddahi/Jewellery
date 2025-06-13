682a141d7130fc8ce56806030fa0e1d6291d500a
BBakkerr <beabakkerr@gmail.com>
2025-06-13 11:59:31 +0200

    a2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,3 +49,27 @@
             <h1>Memorable</h1>
         </div>
     </div>
+
+
+    <div id="sierlijke_lifestyle">
+        <img src="{{asset('images/lifestyle_photos/sierlijke_oorbellen_met_sierlijke_bloemenketting.jpg')}}" alt="Homepage Banner">
+    </div>
+</div>
+@include('layout.nav-products')
+
+<div class="products-container">
+    <div class="product-home">
+        <a href="{{route('categories.show', 'rings')}}"><img src="{{asset('images/rings/enkel_twisted_met_dubbel_twisted_ring.jpg')}}" alt="Rings"></a>
+    </div>
+    <div class="product-home">
+        <a href="{{route('categories.show', 'bracelets')}}"><img src="{{asset('images/bracelets/sierlijke_armband_rotated.jpg')}}" alt="Bracelets"></a>
+    </div>
+    <div class="product-home">
+        <a href="{{route('categories.show', 'necklaces')}}"><img src="{{asset('images/necklaces/sierlijke_bloemenketting.jpg')}}" alt="Necklaces"></a>
+    </div>
+    <div class="product-home">
+        <a href="{{route('categories.show', 'earrings')}}"><img src="{{asset('images/earrings/elegante_oorbellen.jpg')}}" alt="Earrings"></a>
+    </div>
+</div>
+
+@endsection
