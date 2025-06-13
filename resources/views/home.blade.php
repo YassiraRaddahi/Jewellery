24e3e94f39e703c8045a829850d702509c23b6d9
BBakkerr <beabakkerr@gmail.com>
2025-06-13 12:10:34 +0200

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,57 +4,51 @@
 
 
 @section('content')
-
 <div class="homepage_banner">
     <div id="zoekbalk">
-       <form action="{{ route('search') }}" method="GET" id="search-form" > 
+        <form action="{{ route('search') }}" method="GET" id="search-form"> 
             <input 
                 type="text" name="search" id="search-input" placeholder="Search for a product..."  {{ isset($autofocus) && $autofocus ? 'autofocus' : '' }} >
             <button type="submit">
                 <i id="zoekbalk-icon" class="fa-solid fa-magnifying-glass"></i>
             </button>
         </form>
-    </div>
-</div>
 
-
-
-
-            @if(isset($searchResults) && !empty($search))
-            <div id="live-results">
-                @if($searchResults->isEmpty())
-                <p>No results found for "{{ $search }}"</p>
-                @else
-                <ul>
-                    @foreach ($searchResults as $products)
-                    <li class="result-item">
-                        <a href="{{ route('products.show', ['id' => $products->id, 'previous_route' => '/' . request()->path()]) }}"
-                            class="flex items-center gap-2  hover:text-[#c39f86]">
-                            @if(!empty($products->image))
-                            <img src="{{asset($products->image)}}"
-                                class="preview-icon">
-                            @endif
-                            {{ $products->name }}
-                        </a>
-                    </li>
-                    @endforeach
-                </ul>
-                @endif
-            </div>
+        @if(isset($searchResults) && !empty($search))
+        <div id="live-results">
+            @if($searchResults->isEmpty())
+            <p>No results found for "{{ $search }}"</p>
+            @else
+            <ul>
+                @foreach ($searchResults as $products)
+                <li class="result-item">
+                    <a href="{{ route('products.show', ['id' => $products->id, 'previous_route' => '/' . request()->path()]) }}"
+                        class="flex items-center gap-2 hover:text-[#c39f86]">
+                        @if(!empty($products->image))
+                        <img src="{{asset($products->image)}}" class="preview-icon">
+                        @endif
+                        {{ $products->name }}
+                    </a>
+                </li>
+                @endforeach
+            </ul>
             @endif
         </div>
-        <div id="slogan">
-            <h1>Make </h1>
-            <h1>Milestones</h1>
-            <h1>Memorable</h1>
-        </div>
+        @endif
     </div>
 
+    <div id="slogan">
+        <h1>Make </h1>
+        <h1>Milestones</h1>
+        <h1>Memorable</h1>
+    </div>
 
     <div id="sierlijke_lifestyle">
         <img src="{{asset('images/lifestyle_photos/sierlijke_oorbellen_met_sierlijke_bloemenketting.jpg')}}" alt="Homepage Banner">
     </div>
 </div>
+
+
 @include('layout.nav-products')
 
 <div class="products-container">
